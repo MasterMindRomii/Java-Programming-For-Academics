e430f891d8f11011947fe5daf89b14518e31f1ff
Romi Gupta <romi35532@gmail.com>
2024-06-06 12:42:28 +0530

    JDBC Connection Establish Codes

---
+++ JDBC/ConnectionEstablish.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.sql.*;
+ 
+class ConnectionEstablish {
+    public static void main(String[] args) throws Exception
+    {
+        String url = "jdbc:mysql://localhost:3306/student"; // table details
+        String username = "root"; // MySQL credentials
+        String password = "12345";
+        String query= "select *from students"; // query to be run
+        Class.forName("com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(url, username, password);
+        System.out.println("Connection Established successfully");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query); // Execute query
+        rs.next();
+        String name = rs.getString("name"); // Retrieve name from db
+        System.out.println(name); // Print result on console
+        st.close(); // close statement
+        con.close(); // close connection
+        System.out.println("Connection Closed....");
+    }
